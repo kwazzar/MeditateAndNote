65a575f278702d3e269f61154124a1981c50262f
kwazzar <fugitiwe@gmail.com>
2025-02-21 15:07:51 +0200

    Initial Commit

---
+++ MeditateAndNote/MeditateAndNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditateAndNoteApp.swift
+//  MeditateAndNote
+//
+//  Created by Quasar on 21.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MeditateAndNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
